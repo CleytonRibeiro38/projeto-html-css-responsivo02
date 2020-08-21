04690917bfc12b4237029195c17a8e1168159456
Cleyton Ribeiro <jcleytonribeiro@gmail.com>
2020-08-21 17:02:33 -0300

    Ancoragem e Scroll Suave(com JS).

--- projeto/js/funcoes.js
+++ projeto/js/funcoes.js
@@ -30,4 +30,12 @@ $(function () {
         }
     });
 
+    var $doc = $('html,body');
+    $('.scrollSuave').click(function (){
+        $doc.animate({
+            scrollTop:$($.attr(this,'href')).offset().top
+        }, 1000);
+        return false;
+    });
+
 });
