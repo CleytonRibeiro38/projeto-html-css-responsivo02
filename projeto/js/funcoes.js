bffa6316d719d1f5ce7b38828ce144227b3a799e
Cleyton Ribeiro <jcleytonribeiro@gmail.com>
2020-08-25 20:28:14 -0300

    Carregando (com JQuery).

--- projeto/js/funcoes.js
+++ projeto/js/funcoes.js
@@ -66,4 +66,17 @@ $(function () {
         }
     });
 
+    /*==========CARREGANDO==========*/
+    $(window).on('load', function (){
+        document.getElementById("carregando").style.display = "none";
+        document.getElementById("corpo").style.display = "block";
+    });
+
+    //var intervalo = setInterval(function (){
+      //  clearInterval(intervalo);
+
+//        document.getElementById("carregando").style.display = "none";
+  //      document.getElementById("corpo").style.display = "block";
+    //});
+
 });
